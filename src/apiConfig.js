1276b095ea8939b7584a72e3ddb1385a6644aa3e
AWard59 <iamsancho5990@hotmail.com>
2022-01-24 16:48:49 -0800

    fix erroneous / in live url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://reactors-chatterbox.herokuapp.com/',
+  production: 'https://reactors-chatterbox.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
